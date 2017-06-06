99f4f98c79d22d613231960d6ff4fa706230ad75
dekuRockShooter <dekuRockShooter@gmail.com>
2017-06-06 05:12:27 -0700

    Remove some irrelevent comments

--- ui.R
+++ ui.R
@@ -72,27 +72,6 @@ fluidPage(
                   )
            ),
           actionButton('changeDataActionButton', 'Done'),
-  # Sidebar with controls to select the random distribution type
-  # and number of observations to generate. Note the use of the
-  # br() element to introduce extra vertical spacing
-  #sidebarLayout(
-    #sidebarPanel(
-      #radioButtons("dist", "Distribution type:",
-                   #c("Normal" = "norm",
-                     #"Uniform" = "unif",
-                     #"Log-normal" = "lnorm",
-                     #"Exponential" = "exp")),
-      #br(),
-      #
-      #sliderInput("n", 
-                  #"Number of observations:", 
-                   #value = 500,
-                   #min = 1, 
-                   #max = 1000)
-    #),
-    
-    # Show a tabset that includes a plot, summary, and table view
-    # of the generated distribution
           initTabs()
   #)
 )
